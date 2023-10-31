cd2cb16f4224e8649cfdd088dfde2201f7e1047f
Dylan Johnston <qdylanj@gmail.com>
2023-11-01 00:55:08 +0100

    chore: remove unused code in main.go

--- main.go
+++ main.go
@@ -2,55 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	_ "github.com/lib/pq"
 
 	"github.com/joho/godotenv"
 	"github.com/salesfive/yokoy/yokoy"
 )
-
-func getAccessToken() string {
-	baseUrl := os.Getenv("YOKOY_URL")
-	clientId := os.Getenv("CLIENT_ID")
-	clientSecret := os.Getenv("CLIENT_SECRET")
-
-	client := &http.Client{}
-	authUrl := "https://accounts." + baseUrl + "/oauth2/token"
-	v := url.Values{}
-	v.Set("grant_type", "client_credentials")
-	req, err := http.NewRequest("POST", authUrl, strings.NewReader(v.Encode()))
-	req.SetBasicAuth(clientId, clientSecret)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bodyText, err := ioutil.ReadAll(resp.Body)
-	return string(bodyText)
-}
-
-type AddHeaderTransport struct {
-	T http.RoundTripper
-}
-
-func (adt *AddHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Set("Authorization", "Bearer rxDrcrl8cDLKbXi3OXTbvp5D8Pgy8rZfhvI0oPdCJl")
-	req.Header.Set("X-Yk-Auth-Method", "yokoy")
-	return adt.T.RoundTrip(req)
-}
-
-func NewAddHeaderTransport(T http.RoundTripper) *AddHeaderTransport {
-	if T == nil {
-		T = http.DefaultTransport
-	}
-	return &AddHeaderTransport{T}
-}
 
 func main() {
 	db, err := sql.Open("postgres", "dbname=yokoy user=dylan sslmode=disable")
